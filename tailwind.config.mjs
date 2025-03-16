fdf022bbe0b72acbc835d44279f6500028050815
Aheli <aheli@Ahelis-MacBook-Air.local>
2025-03-16 23:43:02 +0530

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        foreground: "rgb(var(--foreground-rgb))",
+        background: "rgb(var(--background-start-rgb))", // You can adjust this to whichever color suits you
       },
     },
   },
